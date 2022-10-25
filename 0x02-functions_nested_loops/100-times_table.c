2d047f4730b3956bbd3bad5c6f0f4ea0f15f198c
ronnymhuka <ronnymhuka@gmail.com>
2022-10-25 00:34:54 -0700

    po

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -34,7 +34,7 @@ void print_times_table(int n)
 				} else if (k >= 100)
 				{
 					_putchar(',');
- 					_putchar(' ');
+					_putchar(' ');
 					_putchar((k / 100) + '0');
 					_putchar(((k / 10) % 10) + '0');
 					_putchar((k % 10) + '0');
@@ -44,4 +44,4 @@ void print_times_table(int n)
 		}
 	}
 }
-				
+
